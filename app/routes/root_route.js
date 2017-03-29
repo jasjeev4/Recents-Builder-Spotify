974c10191422dea0acfcfc61592064f4c83afbd2
Jasjeev <jasjeev4@gmail.com>
2017-03-29 15:31:39 -0400

    Reorganize

--- app/routes/root_route.js
+++ app/routes/root_route.js
@@ -1,5 +1,5 @@
 module.exports = function(express, app) {
-  	app.use( express.static( __dirname + '/client/frontpage' ));
+  	app.use( express.static( __dirname + '/../pages/frontpage' ));
 	app.get('/', function(req, res) {
 		res.sendFile( path.join( __dirname, 'client/frontpage', 'index.html' ));
 	});
