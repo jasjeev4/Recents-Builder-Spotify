e5a55c9077a4384df139ad912443e995efe9fc49
Jasjeev <jasjeev4@gmail.com>
2017-04-20 19:10:05 -0400

    Bugfixes and improvements

--- app/routes/root_route.js
+++ app/routes/root_route.js
@@ -1,6 +1,16 @@
+var path = require('path');
+var cookieParser = require('cookie-parser');
+
 module.exports = function(express, app) {
-  	app.use( express.static( __dirname + '/../pages/frontpage' ));
+	app.use(cookieParser());
+
 	app.get('/', function(req, res) {
-		res.sendFile( path.join( __dirname, 'client/frontpage', 'index.html' ));
+		if(req.cookies.userDetails === undefined) {
+			app.use( express.static( __dirname + '/../pages/frontpage' ));
+			res.sendFile( path.join( __dirname, '/../pages/frontpage', 'index.html' ));
+		}
+		else {
+			res.redirect('/build');
+		}
 	});
 };
