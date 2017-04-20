d3a15494281c30a7438e7e6389451870fb6e6ffa
Jasjeev <jasjeev4@gmail.com>
2017-04-20 19:56:51 -0400

    Add create.js

---
+++ app/routes/create.js
@@ -0,0 +1,10 @@
+var cookieParser = require('cookie-parser');
+var io = require('socket.io')(8088);
+
+module.exports = function(express, app) {
+	app.use(cookieParser());
+
+	app.get('/create', function(req, res) {
+		
+	});
+};
